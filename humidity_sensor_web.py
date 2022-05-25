21f48d7b20fed8dcf5ea891f68c5748c1e3e70ed
hilton92 <benjamin.hilton92@gmail.com>
2022-05-25 15:11:07 -0500

    switching buttons

--- humidity_sensor_web.py
+++ humidity_sensor_web.py
@@ -2,94 +2,127 @@
 # Date: April 2022
 # Humidity Control
 
-import RPi.GPIO as GPIO
 from flask import Flask, render_template
-import time
-import board
-import adafruit_ahtx0
-import csv
 
 app = Flask(__name__)
 
 
-fanRelayPin = 11 
-pumpRelayPin = 13 
-fanOn = False
-pumpOn = False
-overRide = False
-currentHumidity = 45
-currentTemperature = 120
-setHumidity = 45
-
-#i2c = board.I2C()
-#sensor = adafruit_ahtx0.AHTx0(i2c)
-
-GPIO.setup(fanRelayPin, GPIO.OUT)
-GPIO.setup(pumpRelayPin, GPIO.OUT)
-
-
 @app.route('/')
 def index():
+	thisHumidity = get_humidity()
+	thisTemperature = get_temperature()
+	thisFanStatus = get_fan_status()
+	thisPumpStatus = get_pump_status()
+	thisOverride = get_override_status()
+	print("main loop:")
 	templateData = {
 		'title' : 'Humidity Sensor', 
-		'isFanOn' : fanOn,
-		'isPumpOn' : pumpOn,
-		'humidityLevel' : currentHumidity}
+		'isFanOn' : thisFanStatus,
+		'isPumpOn' : thisPumpStatus,
+		'humidityLevel' : thisHumidity,
+		'temperature' : thisTemperature,
+		'overridden' : thisOverride}
 	return render_template('index.html', **templateData)
 
 @app.route("/<deviceName>/<action>")
 def action(deviceName, action):
-	overRide = True
+	set_override()
+	thisHumidity = get_humidity()
+	thisTemperature = get_temperature()
+	thisOverride = get_override_status()
 	if deviceName == 'fan' and action == "on":
 		turn_on_fan()
-	if deviceName == 'fan' and action == "off":
+		thisFanStatus = "on"
+		thisPumpStatus = get_pump_status()
+	elif deviceName == 'fan' and action == "off":
 		turn_off_fan()
-	if deviceName == 'pump' and action == "on":
+		thisFanStatus = "off"
+		thisPumpStatus = get_pump_status()
+	elif deviceName == 'pump' and action == "on":
 		turn_on_pump()
-	if deviceName == 'pump' and action == "off":
+		thisPumpStatus = "on"
+		thisFanStatus = get_fan_status()
+	elif deviceName == 'pump' and action == "off":
 		turn_off_pump()
+		thisPumpStatus = "off"
+		thisFanStatus = get_fan_status()
+	else:
+		thisFanStatus = get_fan_status()
+		thisPumpStatus = get_pump_status()
 	templateData = {
-		'isFanOn' : fanOn,
-		'isPumpOn' : pumpOn,
-		'humidityLevel' : currentHumidity}
+		'isFanOn' : thisFanStatus,
+		'isPumpOn' : thisPumpStatus,
+		'humidityLevel' : thisHumidity,
+		'temperature' : thisTemperature}
 	return render_template('index.html', **templateData)
 
 def get_humidity():
 	with open('data.txt', 'r') as file:
-	return file.readlines()[0]
+		return file.readlines()[0]
 
 def get_temperature():
 	with open('data.txt', 'r') as file:
-	return file.readlines()[1]
+		return file.readlines()[1]
+		
+def get_fan_status():
+	with open('data.txt', 'r') as file:
+		return str(file.readlines()[2])
+		
+def get_pump_status():
+	with open('data.txt', 'r') as file:
+		return str(file.readlines()[3])
+		
+def get_override_status():
+	with open('data.txt', 'r') as file:
+		return file.readlines()[4]
 
 def turn_on_fan():
 	with open('data.txt', 'r') as file:
-	data = file.readlines()
-	data[2] = '1\n'	
+		data = file.readlines()
+		data[2] = 'on\n'	
 
 	with open('data.txt', 'w') as file:
-	file.writelines(data)	
-
-	fanOn = True
-	print("Turning on fan")
+		file.writelines(data)	
+		print("Turning on fan")
 
 def turn_on_pump():	
-	GPIO.output(pumpRelayPin, 0)  #turn on pump by setting low
-	pumpOn = True
-	print("Turning on pump")
-def turn_off_fan():
-	GPIO.output(fanRelayPin, 1) #turn off fan by setting high
-	fanOn = False
-	print("Turning off fan")
-def turn_off_pump():
-	GPIO.output(pumpRelayPin, 1) #turn off pump by setting high
-	pumpOn = False
-	print("Turning off pump")
+	with open('data.txt', 'r') as file:
+		data = file.readlines()
+		data[3] = 'on\n'	
 
+	with open('data.txt', 'w') as file:
+		file.writelines(data)
+		print("Turning on pump")
+	
+def turn_off_fan():
+	with open('data.txt', 'r') as file:
+		data = file.readlines()
+		data[2] = 'off\n'	
+
+	with open('data.txt', 'w') as file:
+		file.writelines(data)
+		print("Turning off fan")
+		
+def turn_off_pump():
+	with open('data.txt', 'r') as file:
+		data = file.readlines()
+		data[3] = 'off\n'	
+
+	with open('data.txt', 'w') as file:
+		file.writelines(data)
+		print("Turning off pump")
+
+def set_override():
+	overRide = True
+	with open('data.txt', 'r') as file:
+		data = file.readlines()
+		data[4] = '1\n'	
+
+	with open('data.txt', 'w') as file:
+		file.writelines(data)
+		print("Setting Override Status")
 
 if __name__ == '__main__':
-	try:
-		app.run(debug = True,threaded=True, port = 80, host = '0.0.0.0')
-	finally:
-		GPIO.cleanup()
+	app.run(debug = True, port = 80, host = '0.0.0.0')
+	
 
